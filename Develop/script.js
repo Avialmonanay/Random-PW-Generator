93a7a6c3c3b96a4cf88b7d06bb876af9929213a2
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-08-01 18:05:17 -0600

    removed user prompt instead added checkboxes and length input directly on the page.

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("Choose password length between 8 and 128")
+
+  console.log ("Generate Button Selected.")
+  console.log (length)
+  console.log (password)
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+
 
 }
 
