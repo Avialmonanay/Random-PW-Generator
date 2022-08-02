4e8d87ee12f00872ae9806db2513f6132317934a
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-08-02 00:27:13 -0600

    event listerner options added. Still unable to generate the password. getting console errors in browser

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,7 @@ const passwordTxt = document.getElementById("password");
 const length = document.getElementById("length");
 const incNumbers = document.getElementById("numbers");
 const incSymbols = document.getElementById("symbols");
-const incupper = document.getElementById("upper");
+const incUpper = document.getElementById("upper");
 
 
 
@@ -21,17 +21,15 @@ var generateBtn = document.querySelector("#generate");
 const writePassword = (length,characters) => {
   var password = "";
   for (let i = 0; i <length; i++) {
-    password += characters.charAt(
-      math.floor(math.random()*characters.length)
-    )
-  }
-
-
-  
-  
-  var passwordText = document.querySelector("#password");
+    password += characters.charAt(math.floor(math.random()*characters.length));
+  };
   passwordText.value = password;
+  console.log = Password
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var characters = base;
+incNumbers.checked ? (characters += numbers) : "";
+incUpper.checked ? (characters += upper) : "";
+incNumbers.checked ? (characters += symbols) : "";
